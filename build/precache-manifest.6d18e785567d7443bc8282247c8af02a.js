1d2ce32894692e65acf004727597543e9b81de40
Colclough7 <you@example.com>
2023-01-26 10:58:28 -0500

    added build folder

---
+++ build/precache-manifest.6d18e785567d7443bc8282247c8af02a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012bf0d3acb9b68481678659a530dc26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c2d96ac0f82f2048b25",
+    "url": "/static/css/main.65ed5060.chunk.css"
+  },
+  {
+    "revision": "ad55c3811081a7fcee7c",
+    "url": "/static/js/2.9491c14d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.9491c14d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c2d96ac0f82f2048b25",
+    "url": "/static/js/main.4de7665f.chunk.js"
+  },
+  {
+    "revision": "c8e5fd3b141acc9e29e7",
+    "url": "/static/js/runtime-main.e51d1157.js"
+  }
+]);
